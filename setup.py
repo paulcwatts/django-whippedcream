57e046711b277e6aa1c48273189336fadd51c739
Paul Watts <paulcwatts@gmail.com>
2014-05-25 09:50:01 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-whippedcream',
-    version='0.1.0',
+    version='0.2.0',
     author='Paul Watts',
     author_email='paulcwatts@gmail.com',
     description='Utilities that make tastypie taste better.',
